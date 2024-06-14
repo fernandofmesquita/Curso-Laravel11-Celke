0228da00cb7cf4dd6633fb3295dc1763e5353da0
Fernando <fernandomesquita@gmail.com>
2024-06-14 15:54:26 -0300

    Inclusão do Framework Bootstrap na pasta public

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <title>Curso de Laravel</title>
 </head>
 <body>
 
     @yield('content')
-    
+
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
 </body>
 </html>
