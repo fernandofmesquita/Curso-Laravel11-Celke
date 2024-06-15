54718a6bf1f652ad5885bb3caf1a9936bd61005e
Fernando <fernandomesquita@gmail.com>
2024-06-15 17:12:07 -0300

    Alteração de Link em todas Views - breadcrumb-item->Dashboard

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -7,7 +7,7 @@
         <h2 class="mt-2">Cursos</h2>
         <ol class="breadcrumb mb-3 mt-3 ms-auto">
             <li class="breadcrumb-item">
-                <a href="#" class="text-decoration-none">Dashboard</a>
+                <a href="/" class="text-decoration-none">Dashboard</a>
             </li>
             <li class="breadcrumb-item active">Cursos</li>
         </ol>
